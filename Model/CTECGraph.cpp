b241e56f10b8a79b158f372082b02ad2b669417a
bleepbloop13 <rbak9218@740s-pl-998.local>
2016-04-25 12:18:12 -0600

    Added the addVertex method

--- Model/CTECGraph.cpp
+++ Model/CTECGraph.cpp
@@ -7,3 +7,29 @@
 //
 
 #include "CTECGraph.hpp"
+
+using namespace CTECData;
+
+template <class Type>
+const int CTECGraph<Type> :: MAXIMUM;
+
+template <class Type>
+void CTECGraph<Type> :: addVertex(const Type& value)
+{
+    assert(size() < MAXIMUM);
+    int newVertexNumber = manyVertices;
+    manyVertices++;
+    
+    for(int iterator = 0; iterator < manyVertices; iterator++)
+    {
+        adjacencyMatrix[iterator][newVertexNumber] = false;
+        adjacencyMatrix[newVertexNumber][iterator] = false;
+    }
+    labels[newVertexNumber] = value;
+}
+
+template <class Type>
+void CTECGraph<Type> :: addEdge(int source, int target)
+{
+    
+}
